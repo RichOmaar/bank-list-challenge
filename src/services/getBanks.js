069474c06758f9492bf8ca29ae622ad4b01798b9
RichOmaar <juanomcam@gmail.com>
2024-03-13 02:16:09 -0600

    CORS

--- src/services/getBanks.js
+++ src/services/getBanks.js
@@ -1,13 +1,26 @@
+// export async function getBanksService() {
+//     // const res = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks', { next: { revalidate: 3600 } });
+//     const res = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks');
+//     const data = await res.json();
+  
+//     return {
+//       props: {
+//         res,
+//         data
+//       },
+//     };
+//   }
+  
 export async function getBanksService() {
-    // const res = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks', { next: { revalidate: 3600 } });
-    const res = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks');
-    const data = await res.json();
-  
-    return {
-      props: {
-        res,
-        data
-      },
-    };
-  }
-  
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl = 'https://dev.obtenmas.com/catom/api/challenge/banks';
+  const res = await fetch(proxyUrl + apiUrl);
+  const data = await res.json();
+
+  return {
+    props: {
+      res,
+      data
+    },
+  };
+}
