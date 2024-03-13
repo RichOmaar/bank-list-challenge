4545779dc51ddbf3c002c51a832e392ba326b650
RichOmaar <juanomcam@gmail.com>
2024-03-13 02:31:56 -0600

    CORS

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/api/:path*",
+                source: "/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
@@ -21,7 +21,7 @@ const nextConfig = {
                 ]
             }
         ]
-    }
+    },
 };
 
 export default nextConfig;
