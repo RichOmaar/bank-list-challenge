56e7595f083314bbd53fb5820231b23ca99ce9aa
RichOmaar <juanomcam@gmail.com>
2024-03-13 02:12:39 -0600

    CORS

--- src/services/getBanks.js
+++ src/services/getBanks.js
@@ -1,5 +1,6 @@
 export async function getBanksService() {
-    const res = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks', { next: { revalidate: 3600 } });
+    // const res = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks', { next: { revalidate: 3600 } });
+    const res = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks');
     const data = await res.json();
   
     return {
